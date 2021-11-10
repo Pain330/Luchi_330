5824245198cf06b5508bbc3de192d8a32e427902
Samuel de Jesús <76408737+Samu330@users.noreply.github.com>
2021-11-10 11:27:49 -0600

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "https://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
-<title>SimSimi - Samu330</title>
+<title>SimSimi Samu330</title>
 <link type="text/css" rel="stylesheet" href="style.css" />
 </head>
 
