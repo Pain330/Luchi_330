d349db1acf2802b117375eb842a4f8706fea353c
Samuel de Jesús <76408737+Samu330@users.noreply.github.com>
2021-11-11 15:42:06 -0600

    Create popup.js

---
+++ popup.js
@@ -0,0 +1,18 @@
+const button = document.querySelector('button');
+const popup = document.querySelector('.popup-wrapper');
+const close = document.querySelector('.popup-close');
+ 
+button.addEventListener('click', () => {
+    popup.style.display = 'block';
+});
+ 
+close.addEventListener('click', () => {
+    popup.style.display = 'none';
+});
+ 
+popup.addEventListener('click', e => {
+    // console.log(e);
+    if(e.target.className === 'popup-wrapper') {
+        popup.style.display = 'none';
+    }
+});
