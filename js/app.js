945a5c5527469c3f06f757ed68d4c13321f54254
Samuel de Jesús <76408737+Samu330@users.noreply.github.com>
2021-11-15 12:50:38 -0600

    Update app.js

--- js/app.js
+++ js/app.js
@@ -23,7 +23,7 @@ particlesJS('particles-js',
         }
       },
       "color": {
-        "value": "#ffffff"
+        "value": "#00ff00"
       },
       "shape": {
         "type": "circle",
@@ -63,7 +63,7 @@ particlesJS('particles-js',
       "line_linked": {
         "enable": true,
         "distance": 150,
-        "color": "#ffffff",
+        "color": "#00ff4d",
         "opacity": 0.4,
         "width": 1
       },
